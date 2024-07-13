4e3b4e7d005e0e4e2ebc01a49943d40d7834da1e
thokar10 <sabin1076@gmail.com>
2024-07-13 09:10:27 +0545

    heelo

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -9,9 +9,9 @@ const BodyPart = () => {
 
       <div className="flex  justify-center md:p-9">
         <div className="gridColumn  grid justify-items-center md:w-[90%] grid-cols-1 gap-4    ">
-          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max  p-2 border-2 border-gray-300  rounded-lg shadow-md">
+          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max md:gap-2  p-2 border-2 border-gray-300  rounded-lg shadow-md">
             <div
-              className="bg-custom h-[20rem] w-[20rem]"
+              className="bg-custom h-[15rem] w-[20rem]"
               style={{
                 backgroundImage: `url(
                   "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -25,9 +25,9 @@ const BodyPart = () => {
             </p>
             <Button className="bg-orange-300 ">Book</Button>
           </div>
-          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 rounded-lg border-2 border-gray-300 shadow-md">
+          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 md:gap-2  rounded-lg border-2 border-gray-300 shadow-md">
             <div
-              className="bg-custom h-[20rem] w-[20rem]"
+              className="bg-custom h-[15rem] w-[20rem]"
               style={{
                 backgroundImage: `url(
                   "https://images.unsplash.com/photo-1601589334680-fb14d7d574f2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -41,9 +41,9 @@ const BodyPart = () => {
             </p>
             <Button className="bg-orange-300 ">Book</Button>
           </div>
-          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2  rounded-lg border-2 border-gray-300 shadow-md">
+          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 md:gap-2   rounded-lg border-2 border-gray-300 shadow-md">
             <div
-              className="bg-custom h-[20rem] w-[20rem]"
+              className="bg-custom h-[15rem] w-[20rem]"
               style={{
                 backgroundImage: `url(
                   "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -58,9 +58,9 @@ const BodyPart = () => {
             <Button className="bg-orange-300 ">Book</Button>
           </div>
 
-          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 border-2 border-gray-300  rounded-lg shadow-md">
+          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 md:gap-2  border-2 border-gray-300  rounded-lg shadow-md">
             <div
-              className="bg-custom h-[20rem] w-[20rem]"
+              className="bg-custom h-[15rem] w-[20rem]"
               style={{
                 backgroundImage: `url(
                   "https://images.unsplash.com/photo-1495562569060-2eec283d3391?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -69,6 +69,38 @@ const BodyPart = () => {
             />
 
             <p>Spain</p>
+            <p>
+              Price <span>Rs.40K</span>
+            </p>
+            <Button className="bg-orange-300 ">Book</Button>
+          </div>
+          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 border-2 border-gray-300  rounded-lg shadow-md">
+            <div
+              className="bg-custom h-[15rem] w-[20rem]"
+              style={{
+                backgroundImage: `url(
+                  "https://images.unsplash.com/photo-1530878902700-5ad4f9e4c318?q=80&w=1934&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                )`,
+              }}
+            />
+
+            <p>Switzerland</p>
+            <p>
+              Price <span>Rs.40K</span>
+            </p>
+            <Button className="bg-orange-300 ">Book</Button>
+          </div>
+          <div className="bg-white flex flex-col font-[Montserrat] items-center justify-between w-max p-2 border-2 md:gap-2  border-gray-300  rounded-lg shadow-md">
+            <div
+              className="bg-custom h-[15rem] w-[20rem]"
+              style={{
+                backgroundImage: `url(
+                  "https://plus.unsplash.com/premium_photo-1679582754365-c68b881bb201?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                )`,
+              }}
+            />
+
+            <p>London</p>
             <p>
               Price <span>Rs.40K</span>
             </p>
